4186ac70563c27d0afc240be4bd9a5b8818e8310
Matt Norman <normanmr@ornl.gov>
2018-10-30 09:14:57 -0400

    now outputting derivs of sfc

--- src/io.cpp
+++ src/io.cpp
@@ -8,7 +8,7 @@
 //The file I/O uses parallel-netcdf, the only external library required for this mini-app.
 //If it's too cumbersome, you can comment the I/O out, but you'll miss out on some potentially cool graphics
 void output_init( str_par &par, str_dom &dom, str_dyn &dyn, str_stat &stat ) {
-  int ncid, t_dimid, x_dimid, y_dimid, height_varid, uvel_varid;
+  int ncid, t_dimid, x_dimid, y_dimid, height_varid, uvel_varid, sfc_x_varid, sfc_y_varid;
   int vvel_varid, fsx_varid, fsy_varid, sfc_varid, t_varid, dimids[3];
   int i, j, num_out, hs;
   long nx, ny;
@@ -35,7 +35,7 @@ void output_init( str_par &par, str_dom &dom, str_dyn &dyn, str_stat &stat ) {
   //Create the file
   ncwrap( ncmpi_create( MPI_COMM_WORLD , "output.nc" , NC_CLOBBER , MPI_INFO_NULL , &ncid ) , __LINE__ );
   //Create the dimensions
-  ncwrap( ncmpi_def_dim( ncid , "t" , (MPI_Offset) NC_UNLIMITED , &t_dimid ) , __LINE__ );
+  ncwrap( ncmpi_def_dim( ncid , "t" , (MPI_Offset) NC_UNLIMITED     , &t_dimid ) , __LINE__ );
   ncwrap( ncmpi_def_dim( ncid , "x" , (MPI_Offset) dom.nx_glob      , &x_dimid ) , __LINE__ );
   ncwrap( ncmpi_def_dim( ncid , "y" , (MPI_Offset) dom.ny_glob      , &y_dimid ) , __LINE__ );
   //Create the variables
@@ -49,13 +49,17 @@ void output_init( str_par &par, str_dom &dom, str_dyn &dyn, str_stat &stat ) {
   ncwrap( ncmpi_def_var( ncid , "fsx"    , NC_DOUBLE , 2 , dimids ,    &fsx_varid ) , __LINE__ );
   ncwrap( ncmpi_def_var( ncid , "fsy"    , NC_DOUBLE , 2 , dimids ,    &fsy_varid ) , __LINE__ );
   ncwrap( ncmpi_def_var( ncid , "sfc"    , NC_DOUBLE , 2 , dimids ,    &sfc_varid ) , __LINE__ );
+  ncwrap( ncmpi_def_var( ncid , "sfc_x"  , NC_DOUBLE , 2 , dimids ,  &sfc_x_varid ) , __LINE__ );
+  ncwrap( ncmpi_def_var( ncid , "sfc_y"  , NC_DOUBLE , 2 , dimids ,  &sfc_x_varid ) , __LINE__ );
   //End "define" mode
   ncwrap( ncmpi_enddef( ncid ) , __LINE__ );
 
   st3[0] = par.j_beg; st3[1] = par.i_beg;
   ct3[0] = ny       ; ct3[1] = nx       ;
-  ncwrap( ncmpi_put_vara_double_all( ncid , fsx_varid , st3 , ct3 , stat.fs_x.get_data() ) , __LINE__ );
-  ncwrap( ncmpi_put_vara_double_all( ncid , fsy_varid , st3 , ct3 , stat.fs_y.get_data() ) , __LINE__ );
+  ncwrap( ncmpi_put_vara_double_all( ncid ,   fsx_varid , st3 , ct3 , stat.fs_x .get_data() ) , __LINE__ );
+  ncwrap( ncmpi_put_vara_double_all( ncid ,   fsy_varid , st3 , ct3 , stat.fs_y .get_data() ) , __LINE__ );
+  ncwrap( ncmpi_put_vara_double_all( ncid , sfc_x_varid , st3 , ct3 , stat.sfc_x.get_data() ) , __LINE__ );
+  ncwrap( ncmpi_put_vara_double_all( ncid , sfc_y_varid , st3 , ct3 , stat.sfc_y.get_data() ) , __LINE__ );
 
   //Store perturbed values in the temp arrays for output
   for (j=0; j<ny; j++) {
