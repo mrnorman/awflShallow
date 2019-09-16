b3a169ca17b5e599ac385b8f5eabc7975e60dd1a
Matt Norman <matthew.ross.norman@gmail.com>
2019-09-16 16:47:54 -0400

    Making TimeIntegrator.h use kernels

--- src/TimeIntegrator.h
+++ src/TimeIntegrator.h
@@ -24,13 +24,14 @@ public :
 
   inline void stepForward(real3d &state, real2d &sfc, Domain &dom, Exchange &exch, Parallel &par) {
     tend.compSWTend(state, sfc, dom, exch, par, tendArr);
-    for (int l=0; l<numState; l++) {
-      for (int j=0; j<dom.ny; j++) {
-        for (int i=0; i<dom.nx; i++) {
-          state(l,hs+j,hs+i) += dom.dt*tendArr(l,j,i);
-        }
-      }
-    }
+    // for (int l=0; l<numState; l++) {
+    //   for (int j=0; j<dom.ny; j++) {
+    //     for (int i=0; i<dom.nx; i++) {
+    Kokkos::parallel_for( numState*dom.ny*dom.nx , KOKKOS_LAMBDA (int iGlob) {
+      int l, j, i;
+      unpackIndices(iGlob,numState,dom.ny,dom.nx,l,j,i);
+      state(l,hs+j,hs+i) += dom.dt*tendArr(l,j,i);
+    });
   }
 
 };
